24fcf6badd73042e9f67783138bb6301c6660e95
n_zhuchkevich <nikitazhuchkevich@gmail.com>
2020-06-25 21:06:06 +0200

    Add empty spring project

---
+++ src/main/java/firstLesson/firstLesson/SimpleController.java
@@ -0,0 +1,12 @@
+package firstLesson.firstLesson;
+
+import org.springframework.stereotype.Controller;
+
+import javax.xml.ws.RequestWrapper;
+
+@Controller
+public class SimpleController {
+
+    @RequestMappingx
+    private void doSomething() {}
+}
---
+++ src/test/java/firstLesson/firstLesson/FirstLessonApplicationTests.java
@@ -0,0 +1,13 @@
+package firstLesson.firstLesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstLessonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
